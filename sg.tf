af69cfc4a42fcc065dc2a7116ac0204a0eb255aa
Diksha Turkar <turkardiksha345@gmail.com>
2026-02-07 20:26:28 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "aws_sg2" {
+  name        = "sg_name"
+  description = "Allow HTTP, SSH inbound and all outbound"
+  vpc_id      = "vpc-0fed28ece99ad2aa5"
+
+  # Allow HTTP (port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow SSH (port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    env = "dev"
+  }
+}
